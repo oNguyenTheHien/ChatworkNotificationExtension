35f5fdc4cb6a7f4d41cc15752079429d5fe7870a
Thanh Nguyen Xuan <thanhfnx@gmail.com>
2018-05-16 17:35:31 +0700

    pull1605

--- Chatwork_notification_extension/background.js
+++ Chatwork_notification_extension/background.js
@@ -6,46 +6,41 @@ var rooms = [];
 var roomsDict = {};
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
   createAlarm();
-  });
 });
 
-var alarmName = 'remindme';
+var alarmName = 'notification-worker';
 
 function createAlarm() {
   chrome.alarms.create(alarmName, {
-    delayInMinutes: 0.1, periodInMinutes: 0.3});
-}
-function cancelAlarm() {
-  chrome.alarms.clear(alarmName);
+    delayInMinutes: 0.0, periodInMinutes: 0.1
+  });
 }
 
 chrome.alarms.onAlarm.addListener(function( alarm ) {
   let tokenStr = localStorage.getItem("insertToken");
   if(tokenStr == "" || tokenStr == null) return;
+  getRooms();
   if (rooms.length == 0)
   {
-    getRooms();
     return;
   }
 
   for (var i = 0; i < rooms.length; i++) {
-    if (rooms[i].type != "group") continue;
-    console.log("room count" + i);
+    console.log("Requesting messages from " + rooms[i].name);
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://api.chatwork.com/v2/rooms/"+rooms[i].room_id+"/messages", false);
+    xmlHttp.open("GET", "https://api.chatwork.com/v2/rooms/"+rooms[i]+"/messages", false);
     xmlHttp.setRequestHeader("X-ChatWorkToken", tokenStr);
     xmlHttp.send(null)
     var currentdate = new Date(); 
     var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                  + (currentdate.getMonth()+1)  + "/" 
-                  + currentdate.getFullYear() + " @ "  
-                  + currentdate.getHours() + ":"  
-                  + currentdate.getMinutes() + ":" 
-                  + currentdate.getSeconds();
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " @ "  
+    + currentdate.getHours() + ":"  
+    + currentdate.getMinutes() + ":" 
+    + currentdate.getSeconds();
     if (xmlHttp.responseText == "" || xmlHttp.responseText == null) {
-      console.log("no message");
+      console.log("No new messages from " + rooms[i].name);
       continue;
     }
     var json = JSON.parse(xmlHttp.responseText);
@@ -55,14 +50,11 @@ chrome.alarms.onAlarm.addListener(function( alarm ) {
     if (length == 0) continue;
     var opt = {
       type: 'basic',
-      title: 'You receive a new message',
+      title: rooms[i].name,
       message: messageBody,
       iconUrl: json[length - 1].account.avatar_image_url
     };
     chrome.notifications.create(datetime,opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
-    if(i == 0) {
-      getRooms();
-    }
   }
 });
 
@@ -81,31 +73,38 @@ function getRooms() {
   if(roomsJson.length == 0) return;
 
   for (var i = 0 ; i < roomsJson.length ; i++) {
-    if(roomsJson[i].unread_num != 0 || roomsDict[roomsJson[i].room_id] == undefined || roomsJson[i].unread_num != roomsDict[roomsJson[i].unread_num]) {
-      rooms.push(roomsJson[i].room_id);
+    var aRoom = roomsJson[i];
+    if (aRoom.type != "group") continue;
+    if (roomsDict[aRoom.room_id] == undefined) {
+      if (aRoom.unread_num != 0) {
+        console.log("Getting messages from " + aRoom.name + ". Unread count: " + aRoom.unread_num);
+        rooms.push(aRoom.room_id);
+      }
+    } else {
+      if (roomsDict[aRoom.room_id] != aRoom.unread_num && aRoom.unread_num != 0) {
+        console.log("Getting messages from " + aRoom.name + ". Unread count: " + aRoom.unread_num);
+        rooms.push(aRoom.room_id);
+      }
     }
-    roomsDict[roomsJson[i].room_id] = roomsJson[i].unread_num;
+    roomsDict[aRoom.room_id] = aRoom.unread_num;
   }
-
 }
 
 
- function convertMessage (messageBody) {
+function convertMessage (messageBody) {
   var index = messageBody.indexOf("[rp aid");
   while (index != -1) {
     for(var i = index; i < messageBody.length; i++) {
       if (messageBody.charAt(i) == ']') {
-        var replyStr = messageBody.substring(index, i+1);
-        messageBody = messageBody.replace(replyStr, " RE:");
+        var replyStr = messageBody.substring(index, i + 1);
+        messageBody = messageBody.replace(replyStr, " [RE]");
         index = messageBody.indexOf("[rp aid");
         break;
       }
     }
   }
-
   return messageBody;
- 
- }
+}
 
 
 
